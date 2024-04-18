3cc559ec066d316ce8d3eaa926a753d1faf127aa
Tamton Aquib <aquibjavedt007@gmail.com>
2024-04-19 01:37:22 +0530

    feat: Frontend initial setup

---
+++ todo-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+	return (
+		<div className='app-container'>
+
+			<h2 className="app-heading">Todo App</h2>
+
+			<div className="project-container">
+				<div className="project">Project1</div>
+				<div className="project">Project2</div>
+				<div className="project">Project3</div>
+				<div className="project">Project4</div>
+				<div className="project">Project5</div>
+			</div>
+
+			{/* <div className="add-project-button"> + </div> */}
+		</div>
+	)
+}
+
+export default App;
