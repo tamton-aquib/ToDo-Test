e72231be05efc4726061f67211c9c9b70a1ea8a1
Tamton Aquib <aquibjavedt007@gmail.com>
2024-04-21 21:20:25 +0530

    feat: edit project title

--- todo-frontend/src/views/Project.jsx
+++ todo-frontend/src/views/Project.jsx
@@ -12,10 +12,13 @@ const Project = () => {
 	const [insertTodo, setInsertTodo] = useState(false);
 	const [input, setInput] = useState('');
 	const [currentProject, setCurrentProject] = useState();
+	const [projectTitle, setProjectTitle] = useState();
+	const [changeProjectTitle, setChangeProjectTitle] = useState(false);
 	console.log("currentProject: ", currentProject);
 
 	useEffect(() => {
 		setCurrentProject(projects.find(p => p.id == projectId));
+		setProjectTitle(currentProject?.title);
 	}, [projects])
 
 	const toggleTodoButton = () => setInsertTodo(p => !p);
@@ -48,22 +51,54 @@ const Project = () => {
 		toggleTodoButton();
 	}
 
+	const handleProjectTitleChange = (e) => {
+		setProjectTitle(e.target.value);
+	}
+
+	const submitProjectTitle = () => {
+		const dataToUpdate = {
+			...currentProject,
+			title: projectTitle
+		}
+
+		fetch('http://localhost:8080/project/update', {
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(dataToUpdate)
+			}).finally(() => console.log("After put"))
+
+		setChangeProjectTitle(p => !p);
+	}
+
 	return (
 		<>
-			<h3>{currentProject?.title}</h3>
-
 			{
-				insertTodo ?
-					(
-						<span>
-							<Input value={input} onChange={handleInputChange} />
-							<Button color='primary' onClick={addTodo}>SUMBIT</Button>
-							<Button color='secondary' onClick={toggleTodoButton}>CANCEL</Button>
-						</span>
-					)
+				changeProjectTitle ?
+					<>
+						<Input value={projectTitle} onChange={handleProjectTitleChange} />
+						<Button onClick={submitProjectTitle}>Submit</Button>
+					</>
 					:
-					<Button onClick={toggleTodoButton}>Add</Button>
+					<span>
+						<span>{projectTitle}</span>
+						<span onClick={() => setChangeProjectTitle(p => !p)}>✏️ </span>
+					</span>
 			}
+
+			<div>
+				{
+					insertTodo ?
+						(
+							<span>
+								<Input value={input} onChange={handleInputChange} />
+								<Button color='primary' onClick={addTodo}>SUMBIT</Button>
+								<Button color='secondary' onClick={toggleTodoButton}>CANCEL</Button>
+							</span>
+						)
+						:
+						<Button onClick={toggleTodoButton}>Add</Button>
+				}
+			</div>
 
 			{currentProject?.todoList?.map(todo => <Todo key={todo.id} todo={todo} />)}
 		</>
